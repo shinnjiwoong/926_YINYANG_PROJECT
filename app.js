86e44de1e1ca5a137e6b931008ee60f185857371
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2024-03-23 13:11:45 +0900

    :wrench: 배경화면 컬러 변경

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let canvas;
 let ctx;
 let isPainting = false;
+let lineWidth = 20;
 
 window.addEventListener('DOMContentLoaded', () => {
     canvas = document.querySelector('canvas');
@@ -9,10 +10,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    changeBGColors();
 });
 window.addEventListener('mousemove', (event) => {
     moveFakeCursor(event.clientX, event.clientY);
-    drawPaint(event);
+    drawPaint(event, lineWidth);
 });
 
 window.addEventListener('mousedown', () => {
@@ -32,21 +35,44 @@ function moveFakeCursor(posX, posY) {
     fakeCursor.style.left = `${width - posX}px`;
 }
 
-function drawPaint(event) {
-    let ogX = event.offsetX;
-    let ogY = event.offsetY;
+function drawPaint(event, lineWidth) {
+    let ogX = event.clientX;
+    let ogY = event.clientY;
 
-    let fakeX = window.innerWidth - event.offsetX;
-    let fakeY = window.innerHeight - event.offsetY;
+    let fakeX = window.innerWidth - event.clientX;
+    let fakeY = window.innerHeight - event.clientY;
 
     if (isPainting == true) {
         console.log('눌러');
         ctx.fillStyle = '#ffffff';
         ctx.beginPath();
-        ctx.ellipse(ogX, ogY, 20, 20, Math.PI / 4, 0, 2 * Math.PI);
-        ctx.ellipse(fakeX, fakeY, 20, 20, Math.PI / 4, 0, 2 * Math.PI);
+        ctx.ellipse(ogX, ogY, lineWidth, lineWidth, Math.PI / 4, 0, 2 * Math.PI);
+        ctx.ellipse(fakeX, fakeY, lineWidth, lineWidth, Math.PI / 4, 0, 2 * Math.PI);
         ctx.fill();
     } else {
         console.log('떼었다');
     }
+}
+
+function changeBGColors() {
+    const colorChangeBtn = document.querySelector('#color-change-btn');
+    const bgWrappers = document.querySelectorAll('.bg-wrapper div');
+
+    colorChangeBtn.addEventListener('click', () => {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+
+        let oppR = 255 - r;
+        let oppG = 255 - g;
+        let oppB = 255 - b;
+
+        bgWrappers[0].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        bgWrappers[1].style.backgroundColor = `rgb(${oppR}, ${oppG}, ${oppB})`;
+    });
+}
+
+function changeLineWidth() {
+    const plus = document.querySelector('#plus');
+    const minus = document.querySelector('#minus');
 }
